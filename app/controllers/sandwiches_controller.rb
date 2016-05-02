ca373a5def59dfbdf78c098ebcfc8ef8b026f87e
Andreza Feu <andrezafeu@gmail.com>
2016-05-02 11:22:27 -0400

    Actions Create and Sandwich_params

--- app/controllers/sandwiches_controller.rb
+++ app/controllers/sandwiches_controller.rb
@@ -3,4 +3,13 @@ class SandwichesController < ApplicationController
 		sandwiches = Sandwich.all
 		render json: sandwiches
 	end
+	def create
+		sandwich = Sandwich.create(sandwich_params)
+		render json: sandwich
+	end
+	private
+	def sandwich_params
+		params.require(:sandwich)
+		.permit(:name, :bread_type)
+	end
 end
