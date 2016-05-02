689b9ff299f4a653abe45e4d2304f78bbbbaca80
Andreza Feu <andrezafeu@gmail.com>
2016-05-02 11:53:42 -0400

    Protect from forgery

--- app/models/sandwich.rb
+++ app/models/sandwich.rb
@@ -1,2 +1,21 @@
 class Sandwich < ActiveRecord::Base
+	def index
+		sandwiches = Sandwich.all
+		render json: sandwiches
+	end
+	def create
+		sandwich = Sandwich.create(sandwich_params)
+		render json: sandwich
+	end
+	private
+	def sandwich_params
+		params.require(:sandwich)
+		.permit(:name, :bread_type)
+	end
+	# {
+	# 	"sandwich": { 
+	# 		"name": "Iron",
+	# 		"bread_type": "White"
+	# 	}
+	# }
 end
