9dcd9034353fcfe740985c0e4c9444ec737dddda
Andreza Feu <andrezafeu@gmail.com>
2016-05-03 15:49:59 -0400

    Add api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :ingredients, only: [:create, :destroy, :show]
-  resources :sandwiches, except: [:new, :edit]
-  #resources : sandwiches, only: [:index, :update, :create, :show, :destroy]
+  resources :sandwiches, only: [:index, :show], controller: "sandwich_views"
+  scope "/api" do
+	  resources :ingredients, only: [:create, :destroy, :show]
+	  resources :sandwiches, except: [:new, :edit]
+	  #resources : sandwiches, only: [:index, :update, :create, :show, :destroy]
+	  post "/sandwiches/:id/ingredients/add" => "sandwiches#add_ingredient"
+	end
 end
